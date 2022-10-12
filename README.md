fea875243655446216687de6546f7dd100e82ca7
abubakkariiui <abubakkarmit@gmail.com>
2022-10-12 23:15:50 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-simple-store-ui
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vmegqa-h7sdxd)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {}
+}
